09149fd77deeb9bd495215b5e53c6d3133c2e946
twesigetrejan <trejantwesige19@gmail.com>
2025-12-15 12:11:27 +0300

    employee login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+// import { Dashboard } from './pages/dashboard/dashboard';
+// import { Layout } from './pages/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  // {
+  //   path: 'dashboard',
+  //   component: Dashboard,
+  // }
+];
