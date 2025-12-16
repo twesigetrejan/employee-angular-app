abe7ad16af1a0bb4a3812429edec34c8a9e6f37f
twesigetrejan <trejantwesige19@gmail.com>
2025-12-16 12:16:58 +0300

    added prime NG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
   ]
 };
